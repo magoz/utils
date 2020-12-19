cd41a1aba39c21cd1153a697a066fcda785616bf
magoz <magoz47@gmail.com>
2020-12-19 18:13:50 +0100

    return false instead of null

--- lastAfter.ts
+++ lastAfter.ts
@@ -1,5 +1,5 @@
 export const lastAfter = (str: string, char: string) => {
   const lastChar = str.lastIndexOf(char)
-  const tail = str.substring(lastChar+1)
-  return tail.length !== str.length ? tail : null
+  if (lastChar === -1) return false
+  return str.substring(lastChar + 1)
 }
